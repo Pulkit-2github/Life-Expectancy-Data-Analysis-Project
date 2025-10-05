429356a4a0f2ccfbf5008e0926f6ebe9ab9b6fbf
Pulkit Mehrotra <166308034+Pulkit-2github@users.noreply.github.com>
2025-10-05 13:43:52 +0530

    app_streamlit.py

---
+++ app_streamlit.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.title("🌍 Life Expectancy Data Analysis Dashboard")
+
+df = pd.read_excel(r"C:\Users\User\Downloads\Life Expectancy Data.xlsx)
+
+st.subheader("Correlation Heatmap")
+fig, ax = plt.subplots(figsize=(12,8))
+sns.heatmap(df.corr(numeric_only=True), cmap='coolwarm', annot=True, linewidths=0.5, ax=ax)
+st.pyplot(fig)
+
+st.subheader("Pairplot (sample)")
+sns.pairplot(df.sample(100))
+st.pyplot()
